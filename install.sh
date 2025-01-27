6ae2c56787fc22849597d5de787bdc6f51c6c897
bepposax <bepposax@hotmail.it>
2025-01-27 20:30:00 +0100

    Add a prompt to create an alias to run the game

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ dependencies=(
     "libncurses-dev"
 )
 
-echo "Checking dependencies..."
+echo -e "Checking dependencies...\n"
 for d in ${dependencies[@]}; do
     if dpkg -s $d &>/dev/null; then
         echo "$d is already installed"
@@ -15,5 +15,30 @@ for d in ${dependencies[@]}; do
         sudo apt install $d && echo "Done"
     fi
 done
+
+echo "---"
+
+read -rp "Do you wish to create the alias 'mines'? [Y/n] " choice
+if [[ $choice = y* || $choice = Y* ]]; then {
+    ALIASFILE=~/.bash_aliases
+    ALIAS="alias mines='cd $(find ~ -type d -name minesweeper-tui) && make run && cd -'"
+
+    # creates ALIASFILE if it doesn't exist
+    [ -f $ALIASFILE ] || {
+    echo -n "Creating '$ALIASFILE'..."
+    touch $ALIASFILE && echo " Done"
+    }
+    # creates the alias 'mines' if it doesn't exist
+    if ! $(grep -q "alias mines" $ALIASFILE 1>/dev/null); then {
+        echo "Appending \"$ALIAS\" to \"$ALIASFILE\""
+        echo "alias mines='cd $(find ~ -type d -name minesweeper-tui) && make run && cd -'" >> ~/.bash_aliases && echo -e "Done\nRestart the terminal for changes to take effect."
+    }
+    else
+        echo "The alias 'mines' is already defined"
+    fi
+} 
+else
+    echo "Abort."
+fi
 
 exit 0
