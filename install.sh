52a0190c2e6a969e9d727655eabca6c59fcd0113
bepposax <bepposax@hotmail.it>
2025-01-28 05:11:51 +0100

    update install script

--- install.sh
+++ install.sh
@@ -6,35 +6,37 @@ dependencies=(
     "libncurses-dev"
 )
 
-echo -e "Checking dependencies...\n"
+GREEN="\033[0;32m"
+RESET="\033[0m"
+
+echo "Checking dependencies..."
 for d in ${dependencies[@]}; do
     if dpkg -s $d &>/dev/null; then
-        echo "$d is already installed"
+        echo -e "$GREEN$d$RESET is already installed"
     else
-        echo "Installing $d..."
+        echo -e "Installing $GREEN$d$RESET..."
         sudo apt install $d && echo "Done"
     fi
 done
 
-echo "---"
-
-read -rp "Do you wish to create the alias 'mines'? [Y/n] " choice
+read -rp "Do you wish to create the alias 'mines' to run the game? [Y/n] " choice
 if [[ $choice = y* || $choice = Y* ]]; then {
     ALIASFILE=~/.bash_aliases
     ALIAS="alias mines='cd $(find ~ -type d -name minesweeper-tui) && make run && cd -'"
 
     # creates ALIASFILE if it doesn't exist
     [ -f $ALIASFILE ] || {
-    echo -n "Creating '$ALIASFILE'..."
+    echo -n "Creating '$ALIASFILE'... "
     touch $ALIASFILE && echo " Done"
     }
     # creates the alias 'mines' if it doesn't exist
     if ! $(grep -q "alias mines" $ALIASFILE); then {
-        echo "Appending \"$ALIAS\" to \"$ALIASFILE\""
-        echo "alias mines='cd $(find ~ -type d -name minesweeper-tui) && make run && cd -'" >> ~/.bash_aliases && echo -e "Done\nRestart the terminal for changes to take effect."
+        echo -n "Appending alias to $ALIASFILE... "
+        echo $ALIAS >> $ALIASFILE &&
+        echo -e "Done\nRestart the terminal for changes to take effect."
     }
     else
-        echo "The alias 'mines' is already defined"
+        echo "Alias mines already exists."
     fi
 } 
 else
